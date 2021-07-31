922991c7024418a317f019c0f5dce66e4bebdbea
Bdey <biplopdey4@gmail.com>
2021-07-31 14:42:48 +0200

    Format code

--- src/Ufo.java
+++ src/Ufo.java
@@ -4,44 +4,44 @@ import java.awt.Rectangle;
 
 public class Ufo extends Element {
 	int UfoCount;
-	
+
 	Ufo(int x, int y, int v, int width, int height) {
 		super(x, y, v, width, height);
-		contadorExplotar=0;
-		UfoCount=0;
-		isLive=false;
-		puntos=50;
+		contadorExplotar = 0;
+		UfoCount = 0;
+		isLive = false;
+		puntos = 50;
 	}
 
 	@Override
-	void mover(int signo){
-		if(UfoCount>Game.frecuenciaOvni) {
-			isLive=true;
-			x=Window.ANCHO;
-			UfoCount=0;
-			contadorExplotar=0;
+	void mover(int signo) {
+		if (UfoCount > Game.frecuenciaOvni) {
+			isLive = true;
+			x = Window.ANCHO;
+			UfoCount = 0;
+			contadorExplotar = 0;
 		}
-		if(x+width<-10) {
-			isLive=false;
+		if (x + width < -10) {
+			isLive = false;
 		}
-		if(isLive) {
-			x+=signo*velocidad;
+		if (isLive) {
+			x += signo * velocidad;
 		}
 		UfoCount++;
 	}
 
 	@Override
 	void pinta(Graphics g) {
-		if(isLive) {
-			g.drawImage(Window.ufo, x, y, width, height, null);	
-		}else if(contadorExplotar<5){
+		if (isLive) {
+			g.drawImage(Window.ufo, x, y, width, height, null);
+		} else if (contadorExplotar < 5) {
 			explotar(g);
 		}
 	}
 
 	void explotar(Graphics g) {
-		if(contadorExplotar<5){
-			g.drawImage(Window.explosionOvni, x, y, width, height, null);	
+		if (contadorExplotar < 5) {
+			g.drawImage(Window.explosionOvni, x, y, width, height, null);
 			contadorExplotar++;
 		}
 	}
